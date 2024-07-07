d78066b71a33c3111aed5675b66e9200bd0fe774
Rabot9ga <freedomboxnikita@inbox.ru>
2024-07-07 12:39:42 +0300

    Rebuilded getting proxies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import sys
 import random
+
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -11,6 +12,10 @@ from logg import ColoredFormatter
 import logging
 from aiohttp_socks import ProxyConnector
 
+logging.basicConfig(level=logging.INFO,
+                    handlers=[logging.StreamHandler()])
+logging.getLogger().handlers[0].setFormatter(ColoredFormatter())
+
 
 async def show(proxies):
     lst = []
@@ -20,33 +25,31 @@ async def show(proxies):
         lst.append(f'{list(proxy.types.keys())[0].lower()}://{proxy.host}:{proxy.port}')
     return lst
 
+
 async def get_url(lst_of_proxy):
     url = 'https://prodoctorov.ru/ufa/ergoterapevt/'
-    type_con = 'socks5'
-    login = 'avVUbUv1'
-    passw = 'kRfNYvKR'
-    proxy_url = '194.87.121.175'
-    port = '64147'
-    # connector = ProxyConnector.from_url(f'{type_con}://{login}:{passw}@{proxy_url}:{port}')
-    for proxy in lst_of_proxy:
-        try:
-            logging.info(proxy)
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, proxy=proxy) as resp:
-                    soup = BeautifulSoup(await resp.text(), 'html.parser')
-                    print(soup.find('a', class_='b-header__logo-stat').text)
-        except Exception as e:
-            logging.error(e)
+    proxy = random.choice(lst_of_proxy)
+    try:
+        logging.info(proxy)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, proxy=proxy) as resp:
+                soup = BeautifulSoup(await resp.text(), 'html.parser')
+                logging.warning(f'Successful proxy: {proxy}')
+                print(soup.find('div', class_='b-header__logo-stat').text.strip())
+    except Exception as e:
+        logging.error(e, f'Failed proxy:  {proxy}')
+        lst_of_proxy.remove(proxy)
+        logging.info(f'length of proxy list: {len(lst_of_proxy)}')
+        await get_url(lst_of_proxy)
+
 
 async def main():
-    logging.basicConfig(level=logging.INFO,
-                        handlers=[logging.StreamHandler()])
-    logging.getLogger().handlers[0].setFormatter(ColoredFormatter())
-    task = asyncio.gather(broker.find(types=['HTTP', 'HTTPS'], limit=20))
+    task = asyncio.gather(broker.find(types=['HTTP', 'HTTPS'], limit=5))
     lst_of_proxy = await show(proxies)
     while not task.done():
         await asyncio.sleep(1)
     await get_url(lst_of_proxy)
+
 
 if __name__ == '__main__':
     proxies = asyncio.Queue()
@@ -55,3 +58,11 @@ if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(task1)
+
+
+# type_con = 'socks5'
+# login = 'avVUbUv1'
+# passw = 'kRfNYvKR'
+# proxy_url = '194.87.121.175'
+# port = '64147'
+# # connector = ProxyConnector.from_url(f'{type_con}://{login}:{passw}@{proxy_url}:{port}')
